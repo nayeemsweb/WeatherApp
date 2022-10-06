8dff9ccd91baaaa60a8fcd892da16b791fddc576
Nayeem Rahman <nayeem12rahman@gmail.com>
2022-10-07 04:08:59 +0600

    Data fetching #1

--- WeatherApp/src/app/app.component.ts
+++ WeatherApp/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WeatherApp';
+export class AppComponent implements OnInit {
+  
+  constructor(private weatherService : WeatherService) {
+
+  }
+  
+  weatherData?: WeatherData;
+  
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('Wellington')
+    .subscribe({
+      next: (response) => {
+        this.weatherData = response;
+        console.log(response);
+      }
+    });
+  }
 }
--- WeatherApp/src/environments/environment.ts
+++ WeatherApp/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  
+  weatherApiBaseUrl: 'https://openweather43.p.rapidapi.com/weather',
+  
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'openweather43.p.rapidapi.com',
+  
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '8a1858cdd4msh238a5139478f0ddp13a12ejsn2518b5283cf4'
+
 };
 
 /*
